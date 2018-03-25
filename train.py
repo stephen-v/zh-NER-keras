fd176a7afe8bb768bf27ed8af2310dc957328ff9
wangsiyu <wangsiyu@cdwww.com>
2018-03-25 14:47:59 +0800

    add batch-size

--- train.py
+++ train.py
@@ -3,5 +3,5 @@ import bilsm_crf_model
 EPOCHS = 3
 model, (train_x, train_y), (test_x, test_y) = bilsm_crf_model.create_model()
 # train model
-model.fit(train_x, train_y, epochs=EPOCHS, validation_data=[test_x, test_y])
+model.fit(train_x, train_y,batch_size=1024,epochs=EPOCHS, validation_data=[test_x, test_y])
 model.save('model/crf.h5')
